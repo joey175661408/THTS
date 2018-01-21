74b5e18936c910eba1c7ab0b9bdd916e3ed7e0cd
Joey <Joey@DESKTOP-IU32QQE>
2018-01-21 22:46:48 +0800

    Add Chart Line

--- THTS/TestCenter/SensorTest.xaml.cs
+++ THTS/TestCenter/SensorTest.xaml.cs
@@ -3,22 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Threading;
+using Microsoft.Research.DynamicDataDisplay.DataSources;
+using Microsoft.Research.DynamicDataDisplay.Charts;
 
 namespace THTS.TestCenter
 {
     public partial class SensorTest
     {
+        ObservableDataSource<Point> _TemperatureCollections = new ObservableDataSource<Point>();
+        /// <summary>
+        /// 温度曲线数据源
+        /// </summary>
+        public ObservableDataSource<Point> TemperatureCollections
+        {
+            get { return _TemperatureCollections; }
+            set { _TemperatureCollections = value;}
+        }
+
+
         public SensorTest()
         {
             InitializeComponent();
+            this.lineTemperature.DataSource = TemperatureCollections;
+        }
+
+        /// <summary>
+        /// 获取实时数据
+        /// </summary>
+        private void GetLiveData()
+        {
+            this.Dispatcher.BeginInvoke((ThreadStart)delegate ()
+            {
+                TemperatureCollections.Collection.Clear();
+            });
+
+            HorizontalDateTimeAxis datetimeAxis = new HorizontalDateTimeAxis();
+
+            while (true)
+            {
+                try
+                {
+                    Thread.Sleep(100);
+                    this.Dispatcher.BeginInvoke((ThreadStart)delegate ()
+                    {
+                        TemperatureCollections.AppendMany(new List<Point> { new Point(datetimeAxis.ConvertToDouble(DateTime.Now), DateTime.Now.Second) });
+                    });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    break;
+                }
+            }
+
+        }
+
+        private void Chart_Click(object sender, RoutedEventArgs e)
+        {
+            Thread thrP = new Thread(new ThreadStart(() =>
+            {
+                GetLiveData();
+            }));
+            thrP.IsBackground = true;
+            thrP.SetApartmentState(ApartmentState.STA);
+            thrP.Start();
         }
     }
 }
---
+++ THTS/TestCenter/SensorTestViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+using THTS.MVVM;
+using Microsoft.Research.DynamicDataDisplay.DataSources;
+using Microsoft.Research.DynamicDataDisplay.Charts;
+
+namespace THTS.TestCenter
+{
+    public class SensorTestViewModel : NotifyObject
+    {
+        ObservableDataSource<Point> _TemperatureCollections = new ObservableDataSource<Point>();
+        /// <summary>
+        /// 温度曲线数据源
+        /// </summary>
+        public ObservableDataSource<Point> TemperatureCollections
+        {
+            get { return _TemperatureCollections; }
+            set { _TemperatureCollections = value; OnPropertyChanged(); }
+        }
+
+        public void SyncData()
+        {
+            Thread thrP = new Thread(new ThreadStart(() =>
+            {
+                GetLiveData();
+            }));
+            thrP.IsBackground = true;
+            thrP.SetApartmentState(ApartmentState.STA);
+            thrP.Start();
+        }
+
+        /// <summary>
+        /// 获取实时数据
+        /// </summary>
+        private void GetLiveData()
+        {
+
+            HorizontalDateTimeAxis datetimeAxis = new HorizontalDateTimeAxis();
+
+            bool isStable = false;
+            DateTime now = DateTime.Now;
+        }
+    }
+}
